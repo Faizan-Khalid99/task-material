258ccab04bcb3b77b0d4026d58bbd8769a2cbb57
Faizan-Khalid99 <86584248+Faizan-Khalid99@users.noreply.github.com>
2022-03-30 15:48:33 +0500

    whitespace del

--- src/store/actions/actions.js
+++ src/store/actions/actions.js
@@ -3,7 +3,6 @@ import { FETCH_MOVIES, DELETE_MOVIE } from "../Types/types";
 export const fetchData = () => async (dispatch) => {
   const response = await fetch(`https://swapi.dev/api/films`);
   const res = await response.json();
-
   dispatch({
     type: FETCH_MOVIES,
     payload: res.results,
